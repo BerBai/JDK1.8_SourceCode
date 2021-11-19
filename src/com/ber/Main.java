e7c07d33ed2a5f30132d14d5a5e0a0980b463a0b
tooyi <bai5775@outlook.com>
2021-11-19 18:08:14 +0800

    'init'

---
+++ src/com/ber/Main.java
@@ -0,0 +1,17 @@
+package com.ber;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("k1", "v1");
+        hashMap.put("k2", "v2");
+        hashMap.put("k3", "v3");
+        hashMap.put("k4", "v4");
+        hashMap.put("k5", "v5");
+        hashMap.get("k2");
+    }
+}
