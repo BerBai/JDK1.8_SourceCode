7461d04e4a422a0543b42d8131582ffbae5f0f00
ber <bai5775@outlook.com>
2022-07-04 14:49:22 +0800

    Serializable

--- src/com/ber/learn/arrayList/test/Serializable.java
+++ src/com/ber/learn/arrayList/test/Serializable.java
@@ -3,6 +3,7 @@ package com.ber.learn.arrayList.test;
 import com.ber.learn.arrayList.model.User;
 
 import java.io.*;
+import java.sql.Array;
 import java.util.ArrayList;
 
 /**
@@ -16,7 +17,9 @@ public class Serializable {
 
     static class SerTest01 {
         public static void main(String[] args) throws IOException, ClassNotFoundException {
+            // 测试序列化写入文件
             writeObject();
+            // 测试反序列化文件读取
             readObject();
         }
 
@@ -34,7 +37,7 @@ public class Serializable {
         // 将对象数据写入文件
         private static void readObject() throws IOException, ClassNotFoundException {
             // 反序列化：将文件中对象的数据读取出来(读对象)
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("..\\obj.txt"));
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("testoutfile\\obj.txt"));
             User user = (User) ois.readObject();
             // 将对象数据写入文件
             System.out.println(user.toString());
@@ -44,29 +47,31 @@ public class Serializable {
     }
 
     public static void main(String[] args) throws Exception {
-        User s = new User();
-        System.out.println(s);
-        //创建对象操作流 --> 序列化
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("MyTest\\obj.txt"));
-        //创建集合,且添加学生对象
+        // 对象操作流 --> 序列化
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("testoutfile\\obj.txt"));
+        oos.writeObject(new String("ces"));
+
+        // 对象输入流 --> 反序列化
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("testoutfile\\obj.txt"));
+        System.out.println((String) ois.readObject());
+
+        // 创建用户对象集合
         ArrayList<User> list = new ArrayList<User>();
-        list.add(new User("悔创阿里杰克马", 51));
-        list.add(new User("会点一点长几颗", 26));
-        list.add(new User("容颜老去蒋青青", 32));
-        list.add(new User("将里最丑刘一飞", 27));
-        //将集合写入到文件
+        list.add(new User("李四", 78));
+        list.add(new User("张三", 28));
+        // 将集合写入到文件
         oos.writeObject(list);
-        //创建对象输入流 --> 反序列化
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("MyTest\\obj.txt"));
-        //读取数据
+        oos.close();
+
+        // 读取数据
         Object o = ois.readObject();
-        //向下转型
+        ois.close();
+        // 向下转型
         ArrayList<User> al = (ArrayList<User>) o;
-        //遍历集合
-        for (int i = 0; i < al.size(); i++) {
-            //根据索引取出集合的每一个元素
-            User stu = al.get(i);
-            System.out.println(stu);
-        }
+
+        // stream流遍历 Lambda
+        al.stream().forEach((user -> System.out.println(user)));
+        // 方法引用
+        al.stream().forEach(System.out::println);
     }
 }
